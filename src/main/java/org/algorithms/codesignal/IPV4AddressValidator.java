59e087890ae624f230e87375de235c99daa18914
Luiz Roberto Freitas <luizrobertofreitas@gmail.com>
2024-01-04 00:33:02 -0300

    Added an ugly IPv4 Validator

---
+++ src/main/java/org/algorithms/codesignal/IPV4AddressValidator.java
@@ -0,0 +1,27 @@
+package org.algorithms.codesignal;
+
+import java.util.function.Predicate;
+
+public final class IPV4AddressValidator {
+    private IPV4AddressValidator() {}
+
+    public static final Predicate<String> SOLUTION = input -> {
+        String[] octets = input.split("\\.");
+        if (octets.length != 4) return false;
+        for (String o : octets) if (!isValidNumber(o)) {
+            return false;
+        }
+        return true;
+    };
+
+    private static boolean isValidNumber(String octet) {
+        try {
+            int o = Integer.parseInt(octet);
+            return (o != 0 || octet.length() <= 1) &&
+                    (o <= 0 || !octet.startsWith("0")) &&
+                    (o >= 0 && o <= 255);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
---
+++ src/test/java/org/algorithms/codesignal/IPV4AddressValidatorTest.java
@@ -0,0 +1,36 @@
+package org.algorithms.codesignal;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class IPV4AddressValidatorTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "172.16.254.1,true",
+            "172.316.254.1,false",
+            ".254.255.0,false",
+            "1.1.1.1a,false",
+            "1,false",
+            "0.254.255.0,true",
+            "1.23.256.255.,false",
+            "1.23.256..,false",
+            "0..1.0,false",
+            "64.233.161.00,false",
+            "64.00.161.131,false",
+            "01.233.161.131,false",
+            "35..36.9.9.0,false",
+            "1.1.1.1.1,false",
+            "1.256.1.1,false",
+            "a0.1.1.1,false",
+            "0.1.1.256,false",
+            "129380129831213981.255.255.255,false",
+            "255.255.255.255abcdekjhf,false",
+            "7283728,false",
+            "0..1.0.0,false",
+    })
+    void test(String input, Boolean expectedResult) {
+        Assertions.assertEquals(expectedResult, IPV4AddressValidator.SOLUTION.test(input));
+    }
+}
