f8b0f58e882a2563e50dfed5076aea56f9bbd120
Luiz Freitas <luiz.freitas@bayer.com>
2024-11-18 21:42:46 -0300

    Added Permutation algo

--- src/test/java/org/algorithms/algs4/module5/assignment/UngradedAssignmentTest.java
+++ src/test/java/org/algorithms/algs4/module5/assignment/UngradedAssignmentTest.java
@@ -1,13 +1,10 @@
 package org.algorithms.algs4.module5.assignment;
 
-import org.algorithms.utils.ArrayCrazyOps;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -15,16 +12,28 @@ class UngradedAssignmentTest {
 
     @ParameterizedTest
     @CsvSource(delimiterString = ";", value = {
-        "10,9,5,4,0,1,7;0,1,12,14,16,4;0,1,4",
-        "1,3,5,7,9,11,13;0,2,4,6,8,10,12;",
-        "1,3,5,7,9,11,13,15,17,19,21,23,25,27,29;2,4,6,8,10,11,13,15,17,19,20,22,24,26;11,13,15,17,19"
+        "10,9,5,4,0,1,7;0,1,12,14,16,4;3",
+        "1,3,5,7,9,11,13;0,2,4,6,8,10,12;0",
+        "1,3,5,7,9,11,13,15,17,19,21,23,25,27,29;2,4,6,8,10,11,13,15,17,19,20,22,24,26;5"
     })
-    void smallSetsTest(String one, String two, String expected) {
+    void smallIntersectSetsTest(String one, String two, Integer expected) {
         Set<Integer> s1 = Arrays.stream(one.split(",")).map(Integer::parseInt).collect(Collectors.toSet());
         Set<Integer> s2 = Arrays.stream(two.split(",")).map(Integer::parseInt).collect(Collectors.toSet());
-        Set<Integer> se = expected == null ?
-            Collections.emptySet() :
-            Arrays.stream(expected.split(",")).map(Integer::parseInt).collect(Collectors.toSet());
-        Assertions.assertEquals(se, UngradedAssignment.INTERSECT.apply(s1, s2));
+        Assertions.assertEquals(expected, UngradedAssignment.INTERSECT.apply(s1, s2).size());
+    }
+
+    @ParameterizedTest
+    @CsvSource(delimiterString = ";", value = {
+        "10,9,5,4,0,1,7;0,1,12,14,16,4;false",
+        "1,3,5,7,9,11,13;0,2,4,6,8,10,12;false",
+        "1,3,5,7,9,11,13,15,17,19,21,23,25,27,29;2,4,6,8,10,11,13,15,17,19,20,22,24,26;false",
+        "1,2,3;3,1,2;true",
+        "2,1,3;3,2,1;true",
+        "1,2,3;3,1,1;false",
+    })
+    void smallArraysPermutationsTest(String one, String two, Boolean expected) {
+        int[] a1 = Arrays.stream(one.split(",")).mapToInt(Integer::parseInt).toArray();
+        int[] a2 = Arrays.stream(two.split(",")).mapToInt(Integer::parseInt).toArray();
+        Assertions.assertEquals(expected, UngradedAssignment.PERMUTATION.test(a1, a2));
     }
 }
